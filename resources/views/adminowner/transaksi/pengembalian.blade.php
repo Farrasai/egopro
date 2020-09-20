fd2e9e4ca4c45988e485c0970399e0e24008e80e
unknown <irvannoorsoleh@gmail.com>
2020-09-20 20:40:39 +0700

    fix download pdf

--- resources/views/adminowner/transaksi/pengembalian.blade.php
+++ resources/views/adminowner/transaksi/pengembalian.blade.php
@@ -298,7 +298,12 @@
                       $.ajax({
                         type: "GET",
                         url: "{{url('/admin/transaksi/pengembalian/cetaknota')}}" + "/" + sewaid,
-                        dataType: "json",
+                        contentType: false,
+                        processData: false,
+                        //xhrFields is what did the trick to read the blob to pdf
+                        xhrFields: {
+                            responseType: 'blob'
+                        },
                         beforeSend: function(){
                           Swal.fire({
                             html: '<br><br><i class="fa fa-circle-o-notch fa-spin" style="font-size:100px"></i><br><br>',
@@ -307,7 +312,50 @@
                         },
                         complete: function(){
                           Swal.close();
-                        }
+                        },
+                        success: function (response, status, xhr) {
+                            var filename = "";                   
+                            var disposition = xhr.getResponseHeader('Content-Disposition');
+
+                            if (disposition) {
+                                var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+                                var matches = filenameRegex.exec(disposition);
+                                if (matches !== null && matches[1]) filename = matches[1].replace(/['"]/g, '');
+                            } 
+                            var linkelem = document.createElement('a');
+                            try {
+                                var blob = new Blob([response], { type: 'application/octet-stream' });                        
+
+                                if (typeof window.navigator.msSaveBlob !== 'undefined') {
+                                    //   IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
+                                    window.navigator.msSaveBlob(blob, filename);
+                                } else {
+                                    var URL = window.URL || window.webkitURL;
+                                    var downloadUrl = URL.createObjectURL(blob);
+
+                                    if (filename) { 
+                                        // use HTML5 a[download] attribute to specify filename
+                                        var a = document.createElement("a");
+
+                                        // safari doesn't support this yet
+                                        if (typeof a.download === 'undefined') {
+                                            window.location = downloadUrl;
+                                        } else {
+                                            a.href = downloadUrl;
+                                            a.download = filename;
+                                            document.body.appendChild(a);
+                                            a.target = "_blank";
+                                            a.click();
+                                        }
+                                    } else {
+                                        window.location = downloadUrl;
+                                    }
+                                }   
+
+                            } catch (ex) {
+                                console.log(ex);
+                            } 
+                          }
                       });
                     }
                   })                  
