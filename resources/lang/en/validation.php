ee094bdc728e62ec3ce25d7225006cdb6e9737b8
TimurDian <timur7dian84@gmail.com>
2020-11-01 20:48:18 +0700

    Add recaptcha

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -133,6 +133,10 @@ return [
         'attribute-name' => [
             'rule-name' => 'custom-message',
         ],
+        'g-recaptcha-response' => [
+            'required' => 'Harap verifikasi bahwa Anda bukan robot',
+            'captcha' => 'Captcha error! try again later or contact site admin.',
+        ],
     ],
 
     /*
